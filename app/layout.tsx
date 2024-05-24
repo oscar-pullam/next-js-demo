0051d1bd433d238a834bb19d86f2be7ac8ba0bf2
oscar-pullam <oscar.pullam@e-gineering.com>
2024-05-24 13:00:03 -0400

    Page titles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
+import { Metadata } from "next";
 import "./ui/global.css";
 import { inter } from '@/app/ui/fonts';
+
+export const metadata: Metadata = {
+  title: {
+    template: 'Acme | %s',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({
   children,
